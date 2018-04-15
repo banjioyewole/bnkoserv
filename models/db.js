fa553e9031994049ab353b0e67aa5c551eeda05e
banjioyewole <efarrari@gmail.com>
2018-04-14 20:54:53 -0700

    post ip_roster

--- models/db.js
+++ models/db.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // var MongoClient = require('mongodb').MongoClient;
 
-var uri = "mongodb+srv://kay:bnkoserv@cluster0.mongodb.net/test";
+// var uri = "mongodb+srv://kay:bnkoserv@cluster0.mongodb.net/test";
 // MongoClient.connect(uri, function(err, client) {
 //    const collection = client.db("test").collection("devices");
 //    // perform actions on the collection object
